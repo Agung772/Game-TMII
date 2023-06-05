e0a8d0d0467cdc94fc2152ac3651e6051df9e47b
Agung Gunawan <agg.nst772@gmail.com>
2023-06-05 09:25:26 +0700

    up

---
+++ Assets/Scripts/Kartu_KartuMemori.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartuMemori_Kartu : MonoBehaviour
+{
+    public Sprite
+        kartuBelakang,
+        KartuDepan;
+
+
+}
