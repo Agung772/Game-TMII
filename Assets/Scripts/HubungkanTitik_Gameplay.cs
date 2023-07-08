83be61ea7e3d4f0fd4090bfdff5150c9e82c03b0
Agung Gunawan <agg.nst772@gmail.com>
2023-07-08 10:36:53 +0700

    up

--- Assets/Scripts/HubungkanTitik_Gameplay.cs
+++ Assets/Scripts/HubungkanTitik_Gameplay.cs
@@ -9,12 +9,8 @@ public class HubungkanTitik_Gameplay : MonoBehaviour
     public int nyawa;
     public int benar;
 
-    public int soalIndex;
-
-    [SerializeField] GameObject[] tugus;
-    public GameObject tugu;
-
-    public GameObject[] soals;
+    public GameObject[] tugu;
+    public GameObject soal;
     private void Awake()
     {
         instance = this;
@@ -22,41 +18,27 @@ public class HubungkanTitik_Gameplay : MonoBehaviour
 
     private void Start()
     {
-        for (int i = 0; i < tugus[1].transform.parent.childCount; i++)
+        for (int i = 0; i < tugu[1].transform.parent.childCount; i++)
         {
-            tugus[1].transform.parent.GetChild(i).gameObject.SetActive(false);
+            tugu[1].transform.parent.GetChild(i).gameObject.SetActive(false);
         }
-
-
 
         //Start
         if (DataGame.instance.pulau == DataGame.instance._Sumatera)
         {
-            tugu = tugus[(int)DataGame.instance.codeTugu];
+            soal = tugu[(int)DataGame.instance.codeTugu];
         }
-
-        tugu.gameObject.SetActive(true);
+        soal.gameObject.SetActive(true);
 
         HubungkanTitik_UI.instance.SetNyawa(nyawa);
-        HubungkanTitik_UI.instance.soalText.text = soalIndex + "/5";
-
-        //Set soall
-        NextSoal();
     }
 
     public void DotBenar()
     {
         benar++;
-        if (tugu.transform.GetChild(0).GetChild(0).childCount == benar)
+        if (soal.transform.GetChild(0).childCount == benar)
         {
-            if (soalIndex == 5)
-            {
-                Minigame_UI.instance.ScoreUI(DataGame.instance.minigame._HubungkanTitik, nyawa);
-            }
-            else
-            {
-                NextSoal();
-            }
+            Minigame_UI.instance.ScoreUI(DataGame.instance.minigame._HubungkanTitik, nyawa);
 
         }
     }
@@ -72,20 +54,4 @@ public class HubungkanTitik_Gameplay : MonoBehaviour
         }
     }
 
-    void NextSoal()
-    {
-        soalIndex++;
-        HubungkanTitik_UI.instance.soalText.text = soalIndex + "/5";
-
-        benar = 0;
-
-        soals = new GameObject[tugu.transform.childCount];
-        for (int i = 0; i < soals.Length; i++)
-        {
-            soals[i] = tugu.transform.GetChild(i).gameObject;
-            soals[i].gameObject.SetActive(false);
-        }
-
-        soals[soalIndex - 1].SetActive(true);
-    }
 }
